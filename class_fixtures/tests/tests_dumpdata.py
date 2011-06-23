93422813593a358cec79adcd9a3f1328bd0fff58
JK Laiho <jklaiho@iki.fi>
2011-06-23 12:13:00 +0300

    Somewhat working fixture code output

--- class_fixtures/tests/tests_dumpdata.py
+++ class_fixtures/tests/tests_dumpdata.py
@@ -27,3 +27,37 @@ class BasicDumpingFunctionalityTests(TestCase):
             fixture_import, model_imports = lines[1], lines[2]
             self.assertEqual(fixture_import, "from class_fixtures.models import Fixture")
             self.assertEqual(model_imports, "from tests.models import Band, Membership, Musician, Roadie")
+
+    def test_correct_fixtures_in_output(self):
+        band = Band.objects.create(name="Brutallica")
+        musician = Musician.objects.create(name="Lars Toorich")
+        membership = Membership.objects.create(band=band, musician=musician, instrument="Bongos", date_joined="1982-01-01")
+        roadie = Roadie.objects.create(name="Ciggy Tardust")
+        roadie.hauls_for.add(band)
+        with string_stdout() as output:
+            call_command('dumpdata', 'tests', format='class', exclude=[
+                'tests.Party', 'tests.Politician'])
+            lines = output.getvalue().split('\n')
+            self.assertEqual(lines[4], 'tests_band_fixture = Fixture(Band)')
+            self.assertEqual(lines[5], 'tests_musician_fixture = Fixture(Musician)')
+            self.assertEqual(lines[6], 'tests_membership_fixture = Fixture(Membership)')
+            self.assertEqual(lines[7], 'tests_roadie_fixture = Fixture(Roadie)')
+    
+    def test_correct_fixture_populating(self):
+        band = Band.objects.create(name="Brutallica")
+        musician = Musician.objects.create(name="Lars Toorich")
+        membership = Membership.objects.create(band=band, musician=musician, instrument="Bongos", date_joined="1982-01-01")
+        roadie = Roadie.objects.create(name="Ciggy Tardust")
+        roadie.hauls_for.add(band)
+        with string_stdout() as output:
+            call_command('dumpdata', 'tests', format='class', exclude=[
+                'tests.Party', 'tests.Politician'])
+            lines = output.getvalue().split('\n')
+            self.assertEqual(lines[9], "tests_band_fixture.add(1, **{'name': u'Brutallica'})")
+            self.assertEqual(lines[10], "tests_musician_fixture.add(1, **{'name': u'Lars Toorich'})")
+            self.assertEqual(lines[11], "tests_membership_fixture.add(1, **{'band': 1, 'date_joined': datetime.date(1982, 1, 1), 'instrument': u'Bongos', 'musician': 1})")
+            self.assertEqual(lines[12], "tests_roadie_fixture.add(1, **{'hauls_for': [1], 'name': u'Ciggy Tardust'})")
+    
+    def test_escaped_characters_in_strings(self):
+        # TODO
+        pass
--- class_fixtures/utils/serialization.py
+++ class_fixtures/utils/serialization.py
@@ -1,13 +1,30 @@
+try:
+    from collections import OrderedDict # Python 2.7 onwards
+except ImportError:
+    from class_fixtures.utils.ordereddict import OrderedDict
+
 from django.core.serializers.base import SerializationError, DeserializationError
 from django.core.serializers.python import _get_model
 from django.db import models
+from django.utils.encoding import smart_unicode
+
+class ClassicReprOrderedDict(OrderedDict):
+    """An OrderedDict subclass with a custom, dict-like __repr__ method."""
+    def __repr__(self):
+        if not self:
+            return '{}'
+        itemlist = []
+        for k,v in self.items():
+            itemlist.append("%r: %r" % (k,v))
+        return '{%s}' % ', '.join(itemlist)
 
 def dump_class_fixtures(objects, stream, **options):
     """
     Generate fixture modules.
     """
-    encoding = 'utf-8'
+    encoding = options.pop('encoding', 'utf-8')
     
+    # Construct and output the import rows
     apps_models = {}
     for identifier in [d['model'] for d in objects]:
         appname = identifier.split('.')[0]
@@ -21,15 +38,32 @@ def dump_class_fixtures(objects, stream, **options):
         if Model._meta.object_name not in apps_models[appname]:
             apps_models[appname].append(Model._meta.object_name)
     
-    model_imports = ''
+    model_imports = []
     for app, models in apps_models.items():
-        # Is it possible for a model to not *also* be available in
-        # appname.models? If yes, remove the hardcoding and get the correct
-        # module by looking at the Model object.
-        model_imports += 'from %s.models import %s\n' % (app, ', '.join(sorted(models)))
+        model_imports.append('from %s.models import %s' % (app, ', '.join(sorted(models))))
     
     stream.write(
         '# -*- coding: {encoding} -*-\n'
         'from class_fixtures.models import Fixture\n'
-        '{imports}\n\n'.format(encoding=encoding, imports=model_imports)
+        '{imports}\n\n'.format(encoding=encoding, imports='\n'.join(model_imports))
     )
+    
+    # Construct and output the Fixture instances
+    fixture_instantiations = []
+    for app, models in apps_models.items():
+        for model in models:
+            fixture_instantiations.append('%s_%s_fixture = Fixture(%s)' % (
+                app, model.lower(), model
+            ))
+    
+    stream.write('\n'.join(fixture_instantiations) + '\n\n')
+    
+    # Construct and output the Fixture.add() calls in the dependency-resolved
+    # order that 'objects' is in. The field names are in the "fields"
+    # dictionary which, being a dictionary, is unordered. To make the output
+    # predictable and testable, we use a subclass of OrderedDict that produces
+    # alphabetized dict-like repr() output.
+    for pk, identifier, fields in [(d['pk'], d['model'], d['fields']) for d in objects]:
+        app, model = identifier.split('.')
+        kwargs = ClassicReprOrderedDict(sorted(fields.items(), key=lambda field: field[0]))
+        stream.write('%s_%s_fixture.add(%s, **%s)\n' % (app, model, pk, repr(kwargs)))
